36b899aecc9cf49c3c3434c9c8ea0d934f68e4f7
thiagolehmam <thiagolehmam@gmail.com>
2024-01-21 16:12:07 -0300

    try / except

--- app.py
+++ app.py
@@ -26,19 +26,25 @@ def opcao_invalida():
     main()
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    # opcao_escolhida = int(opcao_escolhida)
-
-    if opcao_escolhida == 1: 
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2: 
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3: 
-        print('Ativar restaurante')
-    elif opcao_escolhida == 4: 
-        finalizar_app()
-    else:
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
+        
+        if opcao_escolhida == 1: 
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2: 
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3: 
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4: 
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except: 
         opcao_invalida()
+
+
+
 
 def main():
     os.system('cls')
