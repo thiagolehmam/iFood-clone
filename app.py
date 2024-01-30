ff38a58be55cd9ea4ce546c359349bb407ad11f7
thiagolehmam <thiagolehmam@gmail.com>
2024-01-29 21:35:20 -0300

    exibir_subtitulo()

--- app.py
+++ app.py
@@ -20,9 +20,7 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 def finalizar_app():
-    os.system('cls')
-    # os.system('clear') 
-    print('Finalizando o app')
+    exibir_subtitulo('Finalizando o app')
 
 def voltar_ao_menu_princpal():
     input('\nDigite uma tecla para voltar ao menu principal')
@@ -31,19 +29,22 @@ def voltar_ao_menu_princpal():
 def opcao_invalida():
     print('Opção Inválida!\n')
     voltar_ao_menu_princpal()
- 
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)   
+    print() 
 
 def cadastrar_novo_restaurante():
-    os.system('cls')
-    print('cadastro de novos restaurantes\n')
+    
+    exibir_subtitulo('cadastro de novos restaurantes\n')
     nome_do_restaurante = input('Digite o nome do restaurantes que deseja cadastrar:')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!' )
     voltar_ao_menu_princpal()
     
 def listar_restaurantes():
-    os.system('cls')
-    print('Listando os Restaurantes')
+    exibir_subtitulo('Listando os Restaurantes')
 
     for restaurante in restaurantes:
         print(f'.{restaurante}')
